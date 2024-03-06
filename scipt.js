2796c4e85f265dcce8558c34fa4a1d1757f26559
ZephaW <zwamala@gmail.com>
2024-03-06 16:58:33 +0300

    updated JS

--- scipt.js
+++ scipt.js
@@ -6,4 +6,12 @@ const password = document.getElementById('password');
 
 form.addEventListener('submit', e => {
     e.preventDefault ();
+
+
+    var firstName =  fname.value.trim();
+    var lastName =  lname.value.trim();
+    var firstName =  email.value.trim();
+    var passwordValue =  password.value.trim();
+
+     
 })
