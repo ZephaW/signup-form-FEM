4e99bafb65296008fcd98644db15b56f3d2f9deb
ZephaW <zwamala@gmail.com>
2024-03-06 15:40:14 +0300

    iniated script.js file

---
+++ scipt.js
@@ -0,0 +1,9 @@
+const form = document.getElementById('form');
+const fname = document.getElementById('first-name');
+const lname = document.getElementById('last-name');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
+form.addEventListener('submit', e => {
+    e.preventDefault ();
+})
