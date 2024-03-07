c53599dc01d26ade4f9d27a474388321e5bac07b
ZephaW <zwamala@gmail.com>
2024-03-07 12:45:53 +0300

    changes made to js

--- scipt.js
+++ scipt.js
@@ -1,17 +1,9 @@
-const form = document.getElementById('form');
+document.querySelector("submit-button").addEventListener("click", function (event){
+    event.preventDefault
+})
 const fname = document.getElementById('first-name');
 const lname = document.getElementById('last-name');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 
-form.addEventListener('submit', e => {
-    e.preventDefault ();
 
-
-    var firstName =  fname.value.trim();
-    var lastName =  lname.value.trim();
-    var firstName =  email.value.trim();
-    var passwordValue =  password.value.trim();
-
-     
-})
